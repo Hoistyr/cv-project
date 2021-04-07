3f75d0d3d13742956ed4853fb8c5ec1904bd65b2
Hoistyr <hoistyd@gmail.com>
2021-04-07 15:02:00 -0600

    Added edit and delete functions to genInfo

--- src/components/EdInfo.js
+++ src/components/EdInfo.js
@@ -4,39 +4,162 @@ class EdInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      genInfo: {},
+      viewing: false,
+      editing: false,
     }
+
+    this.saveForm = this.saveForm.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  saveForm(event) {
+    console.log('saveForm');
+    event.preventDefault();
+    const genInputs = event.target.parentNode.querySelectorAll('input');
+    console.log(genInputs);
+    let genObject = {};
+    genInputs.forEach((input) => {
+      genObject[input.name] = input.value
+      
+    });
+    console.log(genObject);
+    this.setState({viewing: true, editing: false, genInfo: genObject});
+    
+  }
+
+  handleMouseEnter(event) {
+    let form = event.target;
+    if (!form.classList.contains('genFormHolder')) {
+      form = form.parentNode;
+      if (!form.classList.contains('genFormHolder')) {
+        form = form.parentNode;
+        if (!form.classList.contains('genFormHolder')) {
+          form = form.parentNode;
+        }
+      }
+    }
+    form.classList.add('hoverEdit');
+    const icons = form.querySelectorAll('.actionIcon');
+    icons.forEach((icon) => {
+      icon.classList.remove('hidden');
+    });
+  }
+
+  handleMouseLeave(event) {
+    let form = event.target;
+    
+    if (!form.classList.contains('genFormHolder')) {
+      form = form.parentNode;
+      if (!form.classList.contains('genFormHolder')) {
+        form = form.parentNode;
+        if (!form.classList.contains('genFormHolder')) {
+          form = form.parentNode;
+        }
+      }
+    }
+    
+    form.classList.remove('hoverEdit');
+    const icons = form.querySelectorAll('.actionIcon');
+    icons.forEach((icon) => {
+      icon.classList.add('hidden');
+    });
+  }
+
+  handleEdit(event) {
+    console.log('edit');
+    console.log(this.state.genInfo);
+    this.setState({viewing: false, editing: true});
+  }
+
+  handleDelete(event) {
+    console.log('delete');
+    this.setState({genInfo: {}, viewing: false, editing: false});
   }
   render () {
-    return (
+    const defaultForm = 
+    <div className="edInfoHolder">
       <div className="edInfo">
         <form className="edForm">
             <h1>Education:</h1>
+            
             <label htmlFor="schoolName">
               University Name:
               <input type="text" name="schoolName"/>
             </label>
-            <label htmlFor="degree">
-              Degree:
-              <input type="text" name="degree" defaultValue="Ex: MA, BS, PhD"/>
+            
+            <label htmlFor="city">
+              City:
+              <input type="text" name="city"/>
             </label>
-            <label htmlFor="City">
+
+            <label htmlFor="state">
+              State:
+              <input type="text" name="state"/>
+            </label>
+            
+            <label htmlFor="degreeType">
+              Degree Type:
+              <input type="text" name="degreeType" defaultValue="Ex: MA, BS, PhD"/>
+            </label>
+            
+            <label htmlFor="subject">
               Subject:
               <input type="text" name="subject" defaultValue="Ex: Computer Science, Communication"/>
             </label>
+            
             <label htmlFor="startDate">
             From:
             <input type="date" name="startDate" />
             </label>
+            
             <label htmlFor="endDate">
               To:
               <input type="date" name="endDate" />
             </label>
-            <button className="addEducationBtn">Add</button>
-          <button className="removeEducationBtn">Remove</button>
+            
+            <button className="addEducationBtn" onClick={this.saveForm}>Add</button>
           </form>
+        </div>
       </div>
-    )
+
+    const viewForm = 
+    <div className="edInfoHolder">
+      <div className="edInfo">
+        <form className="edForm">
+            <h1>Education:</h1>
+            <p>{this.state.genInfo.schoolName}</p>
+            <p>{this.state.genInfo.city}</p>
+            <p>{this.state.genInfo.state}</p>
+            <p>{this.state.genInfo.degreeType}</p>
+            <p>{this.state.genInfo.subject}</p>
+            <p>{this.state.genInfo.from}</p>
+            <p>{this.state.genInfo.to}</p>
+        
+            
+            <button className="addEducationBtn" onClick={this.saveForm}>Add</button>
+          </form>
+        </div>
+      </div>
+    
+    let returnForm = '';
+    returnForm = defaultForm;
+    if (this.state.viewing === true && this.state.editing !== true) {
+      returnForm = viewForm;
+    }
+    if (this.state.editing === true && this.state.viewing !== true) {
+      console.log('editing');
+      // returnForm = editForm;
+    }
+
+  return (
+    <div className="edInfo">
+      {returnForm}
+    </div>
+  )
   }
 }
 
--- src/components/GenInfo.js
+++ src/components/GenInfo.js
@@ -6,65 +6,85 @@ class GenInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      genInfo: [],
-      editable: false,
+      genInfo: {},
+      viewing: false,
+      editing: false,
     }
 
     this.saveForm = this.saveForm.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
     this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   saveForm(event) {
     console.log('saveForm');
     event.preventDefault();
-    const genInputs = event.target.parentNode.querySelectorAll('input');
+    const genInputs = event.target.parentNode.querySelectorAll('input' && 'textarea');
     console.log(genInputs);
-    let genValues = [];
     let genObject = {};
     genInputs.forEach((input) => {
       genObject[input.name] = input.value
       
     });
     console.log(genObject);
-    genValues = [...genValues, genObject];
-    console.log(genValues.firstName);
-    this.setState({genInfo: genValues});
-    this.setState({editable: true});
-    console.log(this.state);
+    this.setState({viewing: true, editing: false, genInfo: genObject});
+    
   }
 
   handleMouseEnter(event) {
-    
     let form = event.target;
     if (!form.classList.contains('genFormHolder')) {
-      console.log('1deep');
       form = form.parentNode;
       if (!form.classList.contains('genFormHolder')) {
-        console.log('2deep');
         form = form.parentNode;
+        if (!form.classList.contains('genFormHolder')) {
+          form = form.parentNode;
+        }
       }
     }
-    
     form.classList.add('hoverEdit');
+    const icons = form.querySelectorAll('.actionIcon');
+    icons.forEach((icon) => {
+      icon.classList.remove('hidden');
+    });
   }
 
   handleMouseLeave(event) {
     let form = event.target;
-    if (!form.classList.contains('genForm')) {
-      console.log('1deep');
+    
+    if (!form.classList.contains('genFormHolder')) {
       form = form.parentNode;
-      if (!form.classList.contains('genForm')) {
-        console.log('2deep');
+      if (!form.classList.contains('genFormHolder')) {
         form = form.parentNode;
+        if (!form.classList.contains('genFormHolder')) {
+          form = form.parentNode;
+        }
       }
     }
     
     form.classList.remove('hoverEdit');
+    const icons = form.querySelectorAll('.actionIcon');
+    icons.forEach((icon) => {
+      icon.classList.add('hidden');
+    });
+  }
+
+  handleEdit(event) {
+    console.log('edit');
+    console.log(this.state.genInfo);
+    this.setState({viewing: false, editing: true});
+  }
+
+  handleDelete(event) {
+    console.log('delete');
+    this.setState({genInfo: {}, viewing: false, editing: false});
   }
 
   render () {
-    let genForm = '';
+    console.log('state ', this.state);
+    let returnForm = '';
     
     const defaultForm = 
       <div className="genFormHolder">
@@ -90,56 +110,81 @@ class GenInfo extends React.Component {
               LinkedIn:
               <input type="link" name="linkedIn" />
             </label>
+            <label htmlFor="aboutMe">
+              Additional Information:
+              <textarea name="aboutMe"></textarea>
+            </label>
             <button className="saveGen" onClick={this.saveForm}>Save</button>
         </form>
       </div>
       
 
-      const editForm =
-      <div className="genFormHolder">
+      const viewForm =
+      <div className="genFormHolder" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
         <div className="iconDiv">
-          <div className="editButtonHolder">
-            <img className="editIcon actionIcon" src={editIcon} alt="Click this to edit"></img>
+          <div className="editButtonHolder" onClick={this.handleEdit}>
+            <img className="editIcon actionIcon" src={editIcon} alt="Click this to edit" ></img>
           </div>
-          <div className="deleteButtonHolder">
+          <div className="deleteButtonHolder" onClick={this.handleDelete}>
             <img className="deleteIcon actionIcon" src={deleteIcon} alt="Click this to delete"></img>
           </div>
         </div>
         
-        <form className="genForm" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
-        <h1>General Information:</h1>
-          <label htmlFor="firstName">
-            First Name:
-            <input type="text" name="firstName"/>
-          </label>
-          <label htmlFor="lastName">
-            Last Name:
-            <input type="text" name="lastName"/>
-          </label>
-          <label htmlFor="email">
-            Email:
-            <input type="email" name="email"/>
-          </label>
-          <label htmlFor="phone">
-            Phone Number:
-            <input type="tel" name="phone" />
-          </label>
-          <label htmlFor="linkedIn">
-            LinkedIn:
-            <input type="link" name="linkedIn" />
-          </label>
-        </form>
+        <div className="genForm" >
+          <h1>General Information:</h1>
+          <p className="firstName">{this.state.genInfo.firstName} {this.state.genInfo.lastName}</p>
+          <p>{this.state.genInfo.email}</p>
+          <p>{this.state.genInfo.phone}</p>
+          <p>{this.state.genInfo.linkedIn}</p>
+          <p>{this.state.genInfo.aboutMe}</p>
+        </div>
       </div>
+
+      const editForm = 
+        <div className="genFormHolder-ed">
+          <form className="genForm">
+            <h1>General Information:</h1>
+              <label htmlFor="firstName">
+                First Name:
+                <input type="text" name="firstName" defaultValue={this.state.genInfo.firstName}/>
+              </label>
+              <label htmlFor="lastName">
+                Last Name:
+                <input type="text" name="lastName" defaultValue={this.state.genInfo.lastName}/>
+              </label>
+              <label htmlFor="email">
+                Email:
+                <input type="email" name="email" defaultValue={this.state.genInfo.email}/>
+              </label>
+              <label htmlFor="phone">
+                Phone Number:
+                <input type="tel" name="phone" defaultValue={this.state.genInfo.phone}/>
+              </label>
+              <label htmlFor="linkedIn">
+                LinkedIn:
+                <input type="link" name="linkedIn"  defaultValue={this.state.genInfo.linkedIn}/>
+              </label>
+              <label htmlFor="aboutMe">
+              Additional Information:
+              <textarea name="aboutMe" defaultValue={this.state.genInfo.aboutMe}></textarea>
+              </label>
+              <button className="saveGen" onClick={this.saveForm}>Save</button>
+          </form>
+        </div>
       
 
-      genForm = defaultForm;
-      if (this.state.editable === true) {
-        genForm = editForm;
+      returnForm = defaultForm;
+      if (this.state.viewing === true && this.state.editing !== true) {
+        returnForm = viewForm;
+      }
+      if (this.state.editing === true && this.state.viewing !== true) {
+        console.log('editing');
+        returnForm = editForm;
       }
 
     return (
       <div className="genInfo">
-        {genForm}
+        {returnForm}
       </div>
     )
   }
